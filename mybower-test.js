2fede26d3b65642f9fa39fd682414ed55a03ef31
Anis BOUHACHEM <anis.bouhachem@tritux.com>
2015-12-27 19:54:12 +0100

    Install into directory that has the module name.

--- mybower-test.js
+++ mybower-test.js
@@ -3,6 +3,5 @@ var mybowerConfig = require('./mybower.json');
 
 for (var module in mybowerConfig) {
     if (mybowerConfig[module].local === true) continue;
-    bower.commands.install([mybowerConfig[module].url], {}, {"directory": mybowerConfig[module].directory})
+    bower.commands.install([module + "=" + mybowerConfig[module].url], {}, {"directory": mybowerConfig[module].directory})
 }
-//console.log(bower.commands.install(['jquery'], {}, {"directory": "test"}))
